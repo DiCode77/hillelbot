db0e30a2446a1a95acfb5bf2b8d3d78d480cbcde
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-08-14 22:38:36 +0300

    First attempt at getting updates and sending messages through the bot.

--- src/main.cxx
+++ src/main.cxx
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+#include <fstream>
+#include <sstream>
 
+
+// - Побачити апдейти (кілька повідомлень), що прийшли до бота
+// - Спробувати відправити якесь базове повідомлення назад
 int main() {
+    std::ifstream secret_file("secret.json");
+    bool good = (secret_file.good() && secret_file.is_open());
+
+    if (!good) {
+        std::cout << "ERROR: unable to open the secret file." << std::endl;
+        return -1;
+    }
+
+    nlohmann::json secret_json;
+
+    secret_file >> secret_json;
+    auto secret = secret_json["key"].get<std::string>();
+    secret_file.close();
+
     // Виконати GET-запит до публічного API
-    cpr::Response r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/todos/1"});
+    std::string base_url = (std::stringstream() << "http://api.telegram.org/" << secret).str();
+    cpr::Response r = cpr::Get(cpr::Url{ base_url, "/getUpdates?offset=137803079" });
+    std::cout << "base url? => " << base_url << std::endl;
 
     // Вивести статус код відповіді
     std::cout << "Status Code: " << r.status_code << std::endl;
@@ -13,7 +34,23 @@ int main() {
     nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
 
     // Вивести певне поле з JSON відповіді
-    std::cout << "Title: " << jsonResponse["title"] << std::endl;
+    std::cout << "===JSON===\n" << std::setw(4) << jsonResponse << std::endl;
+    int chat_id = jsonResponse["result"][0]["message"]["chat"]["id"].get<int>();
 
+    cpr::Payload p {
+        {"chat_id", std::to_string(chat_id)},
+        {"text", "HELLO IM BOT!!!1"}
+    };
+
+    std::cout << "Chat ID? => " << chat_id << std::endl;
+
+    cpr::Response postR = cpr::Post(cpr::Url{ base_url, "/sendMessage" }, p);
+    std::cout << "Post Status Code? => " << postR.status_code << std::endl;;
+    // Парсити JSON відповідь
+    nlohmann::json postResponse = nlohmann::json::parse(postR.text);
+
+    // Вивести певне поле з JSON відповіді
+    std::cout << "===JSON===\n" << std::setw(4) << postResponse << std::endl;
+ 
     return 0;
 }
